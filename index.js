d9cdeb04b76af34d1cf237d445bcd6d412cd98dc
skalum <stkalum@gmail.com>
2017-09-29 19:35:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var katzDeli = [];
+var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
 
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+
